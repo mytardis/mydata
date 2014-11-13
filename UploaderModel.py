c67932b9a158b8b95cde37c9babc1f4976983d86
James Wettenhall <james.wettenhall@monash.edu>
2014-11-13 16:17:19 +1100

    Updated comments.

--- UploaderModel.py
+++ UploaderModel.py
@@ -7,6 +7,17 @@ The purpose of this module is to help with registering MyData uploaders
 Every time MyData runs, or when its settings are updated, it POSTs to its
 preferred MyTardis server some basic information about the PC and about the
 MyData installation.  This basic information is called an "uploader" record.
+This initial POST doesn't require any authentication, but once an uploader
+record has been created in MyTardis, then no other users (of MyTardis's
+RESTful API) will be able to access the uploader record unless they know
+its MAC address, a unique string associated with the MyData user's network
+interface (Ethernet or WiFi or ...).  A single MyData user could create
+multiple uploader records from each PC they run MyData on, one for each
+network interface on each PC.  The primary reason for allowing the initial
+POST without authentication is because (at least for now), the MyTardis
+administrators are keen to be notified of any users attempting to use
+MyData, even if those users can't figure out how to configure
+authentication in MyData.
 
 Initially only HTTP POST uploads are enabled in MyData, but MyData will request
 uploads via RSYNC over SSH to a staging area, and wait for a MyTardis
@@ -24,7 +35,7 @@ $ chmod 700 /home/mydata/.ssh/
 $ chmod 600 /home/mydata/.ssh/authorized_keys
 $ usermod -a -G mytardis mydata
 
-N.B.: The test below is only possible because the MyData user submitting the
+N.B.: The test below was only possible because the MyData user submitting the
 request and the MyTardis administrator approving the request were the same
 person.  Normally, the MyTardis administrator wouldn't have access to the
 MyData user's private key.
@@ -38,8 +49,8 @@ mydata mytardis
 [mydata@118.138.241.33 ~]$ ls -lh /mnt/sonas/market | grep MYTARDIS
 drwx------ 403 mytardis mytardis 128K Nov 12 14:33 MYTARDIS_FILE_STORE
 drwxrwx---   3 mytardis mytardis  32K Nov 13 15:36 MYTARDIS_STAGING
-[mydata@118.138.241.33 ~]$ touch /mnt/sonas/market/MYTARDIS_STAGING/testing123.txt
-[mydata@118.138.241.33 ~]$ rm /mnt/sonas/market/MYTARDIS_STAGING/testing123.txt
+[mydata@118.138.241.33 ~]$ touch /mnt/sonas/market/MYTARDIS_STAGING/test123.txt
+[mydata@118.138.241.33 ~]$ rm /mnt/sonas/market/MYTARDIS_STAGING/test123.txt
 
 Note the permissions above - being part of the "mytardis" group on this staging
 host allows the "mydata" user to write to the MYTARDIS_STAGING directory, but
