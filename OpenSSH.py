af1dc4f94b8ca04c5f67294afa557a0cd798146c
James Wettenhall <james.wettenhall@monash.edu>
2014-12-01 00:43:20 +1100

    New CAT over SSH upload method.  Currently it just appends one chunk at a time onto the end of the remote file (in the staging area). It would be better to upload the chunk to staging first, and then append it, so then even if the connection is broken, we would still hopefully end up with a partial staging datafile which is a whole number of chunks in size.

--- OpenSSH.py
+++ OpenSSH.py
@@ -6,13 +6,13 @@ from os.path import expanduser
 import re
 
 from logger.Logger import logger
+from Exceptions import SshException
 
 
 class OpenSSH():
-
     OPENSSH_BUILD_DIR = 'openssh-cygwin-stdin-build'
 
-    def doubleQuote(self, x):
+    def DoubleQuote(self, x):
         return '"' + x + '"'
 
     def __init__(self):
@@ -26,30 +26,62 @@ class OpenSSH():
 
         if sys.platform.startswith("win"):
             if hasattr(sys, "frozen"):
-                f = lambda x: os.path.join(os.path.dirname(sys.executable),
-                                           self.OPENSSH_BUILD_DIR, "bin", x)
+                f = lambda x, y: os.path.join(os.path.dirname(sys.executable),
+                                              self.OPENSSH_BUILD_DIR, x, y)
             else:
                 try:
                     mydataModulePath = \
                         os.path.dirname(pkgutil.get_loader("MyData").filename)
                 except:
                     mydataModulePath = os.getcwd()
-                f = lambda x: os.path.join(mydataModulePath,
-                                           self.OPENSSH_BUILD_DIR, "bin", x)
-            self.ssh = self.doubleQuote(f("ssh.exe"))
-            self.sshKeyGen = self.doubleQuote(f("ssh-keygen.exe"))
-            self.chown = self.doubleQuote(f("chown.exe"))
-            self.chmod = self.doubleQuote(f("chmod.exe"))
+                f = lambda x, y: os.path.join(mydataModulePath,
+                                              self.OPENSSH_BUILD_DIR, x, y)
+            self.ssh = f("bin", "ssh.exe")
+            self.sshKeyGen = f("bin", "ssh-keygen.exe")
+            self.chown = f("bin", "chown.exe")
+            self.chmod = f("bin", "chmod.exe")
+            self.chgrp = f("bin", "chgrp.exe")
+            self.cygpath = f("bin", "cygpath.exe")
+            # When using bundled Cygwin binaries for SSH, we need to
+            # ensure that ownership and permissions of the private
+            # key file are correct (from Cygwin's perspective). Using
+            # Cygwin's "ls.exe" binary with "-l" # is more accurate
+            # at measuring the relevant permissions than os.stat().
+            self.ls = f("bin", "ls.exe")
+            self.mkgroup = f("bin", "mkgroup.exe")
+            self.etcGroupPath = f("etc", "group")
+
+            with open(self.etcGroupPath, "w") as etcGroup:
+                cmdAndArgs = [self.mkgroup, "-l"]
+                proc = subprocess.Popen(cmdAndArgs,
+                                        stdin=subprocess.PIPE,
+                                        stdout=etcGroup,
+                                        stderr=subprocess.STDOUT,
+                                        universal_newlines=True,
+                                        shell=True,
+                                        startupinfo=defaultStartupInfo,
+                                        creationflags=defaultCreationFlags)
+                stdout, stderr = proc.communicate()
+                if proc.returncode != 0:
+                    raise SshException(stderr)
+
+            self.cipher = "arcfour"
         elif sys.platform.startswith("darwin"):
             self.ssh = "/usr/bin/ssh"
             self.sshKeyGen = "/usr/bin/ssh-keygen"
             self.chown = "/usr/sbin/chown"
             self.chmod = "/bin/chmod"
+            self.chgrp = "/bin/chgrp"
+            self.ls = "/bin/ls"
+            self.cipher = "arcfour128"
         else:
             self.ssh = "/usr/bin/ssh"
             self.sshKeyGen = "/usr/bin/ssh-keygen"
             self.chown = "/bin/chown"
             self.chmod = "/bin/chmod"
+            self.chgrp = "/bin/chgrp"
+            self.ls = "/bin/ls"
+            self.cipher = "arcfour128"
 
 
 class KeyPair():
@@ -68,11 +100,14 @@ class KeyPair():
     def __repr__(self):
         return self.__unicode__()
 
-    def publicKey(self):
+    def GetPrivateKeyFilePath(self):
+        return self.privateKeyFilePath
+
+    def ReadPublicKey(self):
         with open(self.publicKeyFilePath, "r") as pubKeyFile:
             return pubKeyFile.read()
 
-    def delete(self):
+    def Delete(self):
         try:
             os.unlink(self.privateKeyFilePath)
             if self.publicKeyFilePath is not None:
@@ -83,8 +118,82 @@ class KeyPair():
 
         return True
 
+    def CheckPermissions(self):
+        permissions = None
+        owner = None
+        group = None
+        cmdAndArgs = [ls, "-l",
+                      GetCygwinPath(self.privateKeyFilePath)]
+        logger.debug(str(cmdAndArgs))
+        proc = subprocess.Popen(cmdAndArgs,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                startupinfo=defaultStartupInfo,
+                                creationflags=defaultCreationFlags)
+        stdout, stderr = proc.communicate()
+        logger.debug(stdout)
+        for line in stdout.splitlines():
+            match = re.search(r"^(\S+)\s+\d+\s+(\S+)\s+(\S+)", line)
+            if match:
+                permissions = match.groups()[0]
+                owner = match.groups()[1]
+                group = match.groups()[2]
+                break
+        if not match:
+            raise SshException("Failed to check permissions on "
+                               "private key file.")
+        match1 = re.search(r"^-rw-------", permissions)
+        match2 = re.search(r"^-r--------", permissions)
+        if match1 or match2:
+            logger.info("Permissions on private key file look OK: %s"
+                        % permissions)
+            return True
 
-def listKeyPairs(keyPath=None):
+        if sys.platform.startswith("win"):
+            if group == "None" or group == "mkpasswd":
+                logger.error("Group is wrong on private key file: %s"
+                             % group)
+                if os.path.exists(etcGroupPath):
+                    logger.debug("Group file exists: %s" % etcGroupPath)
+                else:
+                    logger.debug("Group file doesn't exist. "
+                                 "Need to run %s" % mkgroup)
+                    raise SshException("Missing group file: %s", etcGroupPath)
+
+                cmdAndArgs = [chgrp, "Users",
+                              GetCygwinPath(self.privateKeyFilePath)]
+                logger.debug(str(cmdAndArgs))
+                proc = subprocess.Popen(cmdAndArgs,
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT,
+                                        shell=True,
+                                        startupinfo=defaultStartupInfo,
+                                        creationflags=defaultCreationFlags)
+                stdout, stderr = proc.communicate()
+                if proc.returncode != 0:
+                    logger.error(stdout)
+                    raise SshException("Failed to change private key "
+                                       "file's group")
+                logger.info("Set private key file's group to \"Users\".")
+
+                cmdAndArgs = [chmod, "600",
+                              GetCygwinPath(self.privateKeyFilePath)]
+                logger.debug(str(cmdAndArgs))
+                proc = subprocess.Popen(cmdAndArgs,
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT,
+                                        startupinfo=defaultStartupInfo,
+                                        creationflags=defaultCreationFlags)
+                stdout, stderr = proc.communicate()
+                if proc.returncode != 0:
+                    raise SshException(stderr)
+                logger.info("Set private key file's permissions to 600.")
+
+
+def ListKeyPairs(keyPath=None):
     if keyPath is None:
         keyPath = os.path.join(expanduser('~'), ".ssh")
     filesInKeyPath = [f for f in os.listdir(keyPath)
@@ -106,7 +215,7 @@ def listKeyPairs(keyPath=None):
     return keyPairs
 
 
-def findKeyPair(keyName, keyPath=None):
+def FindKeyPair(keyName, keyPath=None):
     if keyPath is None:
         keyPath = os.path.join(expanduser('~'), ".ssh")
     if os.path.exists(os.path.join(keyPath, keyName)):
@@ -131,7 +240,7 @@ if sys.platform.startswith("win"):
     defaultCreationFlags = win32process.CREATE_NO_WINDOW
 
 
-def newKeyPair(keyName=None,
+def NewKeyPair(keyName=None,
                keyPath=None,
                keyComment=None,
                startupinfo=defaultStartupInfo,
@@ -150,17 +259,16 @@ def newKeyPair(keyName=None,
     if sys.platform.startswith('win'):
         cmdList = \
             [sshKeyGen, "-f",
-             openSSH.doubleQuote(privateKeyFilePath),
-             "-C", openSSH.doubleQuote(keyComment)]
+             openSSH.DoubleQuote(privateKeyFilePath),
+             "-C", openSSH.DoubleQuote(keyComment)]
         cmd = " ".join(cmdList)
         proc = subprocess.Popen(cmd,
                                 stdin=subprocess.PIPE,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT,
-                                universal_newlines=True,
                                 shell=True,
-                                startupinfo=startupinfo,
-                                creationflags=creationflags)
+                                startupinfo=defaultStartupInfo,
+                                creationflags=defaultCreationFlags)
         stdout, stderr = proc.communicate()
 
         if stdout is None or str(stdout).strip() == "":
@@ -169,8 +277,8 @@ def newKeyPair(keyName=None,
         elif "Your identification has been saved" in stdout:
             success = True
         elif "already exists" in stdout:
-            raise Exception("Private key file \"%s\" already exists."
-                            % privateKeyFilePath)
+            raise SshException("Private key file \"%s\" already exists."
+                               % privateKeyFilePath)
         else:
             logger.error("OpenSSH.newKeyPair: "
                          "Got unknown error from ssh-keygen binary")
@@ -179,15 +287,15 @@ def newKeyPair(keyName=None,
         import pexpect
 
         args = ["-f", privateKeyFilePath,
-                "-C", openSSH.doubleQuote(keyComment)]
+                "-C", openSSH.DoubleQuote(keyComment)]
         lp = pexpect.spawn(sshKeyGen, args=args)
         idx = lp.expect(["already exists",
                         pexpect.EOF])
         if idx == 0:
-            raise Exception("Private key file \"%s\" already exists.")
+            raise SshException("Private key file \"%s\" already exists.")
         else:
-            raise Exception("Unexpected result from "
-                            "attempt to create new key.")
+            raise SshException("Unexpected result from "
+                               "attempt to create new key.")
         lp.close()
     publicKeyFilePath = privateKeyFilePath + ".pub"
 
@@ -197,9 +305,130 @@ def newKeyPair(keyName=None,
         return None
 
 
-openSSH = OpenSSH()
+def GetCygwinPath(path):
+    if not path.startswith('"'):
+        path = openSSH.DoubleQuote(path)
+    cmdList = [cygpath] + [path]
+    cmd = " ".join(cmdList)
+    proc = subprocess.Popen(cmd,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            shell=True,
+                            startupinfo=defaultStartupInfo,
+                            creationflags=defaultCreationFlags)
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception(stderr)
+    cygwinPath = stdout.strip()
+    return cygwinPath
 
+
+def GetBytesUploadedToStaging(remoteFilePath, username, privateKeyFilePath,
+                              hostname):
+    if not remoteFilePath.startswith('"'):
+        remoteFilePath = openSSH.DoubleQuote(remoteFilePath)
+    cmdAndArgs = [ssh,
+                  "-i", privateKeyFilePath,
+                  "-l", username,
+                  "-c", openSSH.cipher,
+                  "-oStrictHostKeyChecking=no",
+                  hostname,
+                  "wc -c %s" % remoteFilePath]
+    cmdString = " ".join(cmdAndArgs)
+    logger.debug(cmdString)
+    proc = subprocess.Popen(cmdAndArgs,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            startupinfo=defaultStartupInfo,
+                            creationflags=defaultCreationFlags)
+    stdout, stderr = proc.communicate()
+    lines = stdout.splitlines()
+    bytesUploaded = 0
+    try:
+        for line in lines:
+            match = re.search(r"^(\d+)\s+\S+", line)
+            if match:
+                bytesUploaded = long(match.groups()[0])
+                break
+    except:
+        logger.debug(stdout)
+        logger.debug(traceback.format_exc())
+    return bytesUploaded
+
+
+def UploadFile(filePath, fileSize, username, privateKeyFilePath,
+               hostname, remoteFilePath, progressCallback,
+               foldersController, uploadModel):
+    if not remoteFilePath.startswith('"'):
+        remoteFilePath = openSSH.DoubleQuote(remoteFilePath)
+    # The file may have already been uploaded, so let's check
+    # for it (and check its size) on the server. We don't use
+    # checksums here because they are slow for large files,
+    # and ultimately it will be the MyTardis verification
+    # task which does the final check.
+    bytesUploaded = GetBytesUploadedToStaging(remoteFilePath,
+                                              username, privateKeyFilePath,
+                                              hostname):
+    if bytesUploaded == fileSize:
+        logger.debug("UploadFile returning because file \"%s\" has already "
+                     "been uploaded." % filePath)
+        return
+    elif bytesUploaded > fileSize:
+        logger.error("Possibly due to a bug in MyData, the file size on "
+                     "the remote server is larger than the local file size "
+                     "for \"%s\"." % filePath)
+    elif 0 < bytesUploaded < fileSize:
+        logger.warning("FIXME: MyData should be able to resume partially "
+                       "completed uploads, but for now, we'll restart the "
+                       "upload for \"%s\"..." % filePath)
+    elif bytesUploaded == 0:
+        # The most common use case.
+        pass
+
+    cmdAndArgs = [ssh,
+                  "-i", privateKeyFilePath,
+                  "-l", username,
+                  "-c", openSSH.cipher,
+                  "-oStrictHostKeyChecking=no",
+                  hostname,
+                  "/bin/rm -f %s && cat >> %s"
+                  % (remoteFilePath, remoteFilePath)]
+    cmdString = " ".join(cmdAndArgs)
+    logger.debug(cmdString)
+    proc = subprocess.Popen(cmdAndArgs,
+                            stdin=subprocess.PIPE,
+                            startupinfo=defaultStartupInfo,
+                            creationflags=defaultCreationFlags)
+    chunkSize = 102400
+    oneMegabyte = 1048576
+    while (fileSize / chunkSize) > 100 and chunkSize < oneMegabyte:
+        chunkSize = chunkSize * 2
+    with open(filePath, 'rb') as fp:
+        bytesWritten = 0
+        for chunk in iter(lambda: fp.read(chunkSize), b''):
+            if foldersController.IsShuttingDown() or uploadModel.Canceled():
+                logger.debug("Aborting upload for "
+                             "%s" % filePath)
+                proc.terminate()
+                return None
+            proc.stdin.write(chunk)
+            bytesWritten += len(chunk)
+            progressCallback(None, bytesWritten, fileSize)
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        logger.error(cmdString)
+        logger.error(stdout)
+        raise SshException(stdout)
+
+openSSH = OpenSSH()
 ssh = openSSH.ssh
 sshKeyGen = openSSH.sshKeyGen
 chown = openSSH.chown
 chmod = openSSH.chmod
+chgrp = openSSH.chgrp
+ls = openSSH.ls
+if sys.platform.startswith("win"):
+    cygpath = openSSH.cygpath
+    etcGroupPath = openSSH.etcGroupPath
+    mkgroup = openSSH.mkgroup
