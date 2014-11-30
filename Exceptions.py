580540ca1802b96aed73227f185112b0956cc7fd
James Wettenhall <james.wettenhall@monash.edu>
2014-11-30 21:47:39 +1100

    Some new Exception classes to help with managing common HTTP error codes returned by MyTardis's API.

--- Exceptions.py
+++ Exceptions.py
@@ -1,5 +1,73 @@
-class DuplicateKeyException(Exception):
+class DuplicateKey(Exception):
     def __init__(self, message):
 
         # Call the base class constructor with the parameters it needs
-        super(DuplicateKeyException, self).__init__(message)
+        super(DuplicateKey, self).__init__(message)
+
+
+class MultipleObjectsReturned(Exception):
+    def __init__(self, message, url, response):
+
+        # Call the base class constructor with the parameters it needs
+        super(MultipleObjectsReturned, self).__init__(message)
+
+        self.url = url
+        self.response = response
+
+    def GetUrl(self):
+        return self.url
+
+    def GetResponse(self):
+        return self.response
+
+
+class DoesNotExist(Exception):
+    def __init__(self, message, url=None, response=None):
+
+        # Call the base class constructor with the parameters it needs
+        super(DoesNotExist, self).__init__(message)
+
+        self.url = url
+        self.response = response
+
+    def GetUrl(self):
+        return self.url
+
+    def GetResponse(self):
+        return self.response
+
+class Unauthorized(Exception):
+    def __init__(self, message, url=None, response=None):
+
+        # Call the base class constructor with the parameters it needs
+        super(Unauthorized, self).__init__(message)
+
+        self.url = url
+        self.response = response
+
+    def GetUrl(self):
+        return self.url
+
+    def GetResponse(self):
+        return self.response
+
+class InternalServerError(Exception):
+    def __init__(self, message, url=None, response=None):
+
+        # Call the base class constructor with the parameters it needs
+        super(InternalServerError, self).__init__(message)
+
+        self.url = url
+        self.response = response
+
+    def GetUrl(self):
+        return self.url
+
+    def GetResponse(self):
+        return self.response
+
+class SshException(Exception):
+    def __init__(self, message):
+
+        # Call the base class constructor with the parameters it needs
+        super(SshException, self).__init__(message)
