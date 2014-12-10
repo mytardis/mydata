db0ff3ed36fe9cd7b0d93000711f8a0f0e55c62b
James Wettenhall <james.wettenhall@monash.edu>
2014-12-10 13:58:44 +1100

    Adding server-side scripts for receiving POSTed debug logs and for emailing debug logs to MyTardis administrators.

---
+++ logger/email-mydata-debug-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This file, used by MyData, lives in
+# /root/cron/email-mydata-debug-logs.sh
+# on cvl.massive.org.au
+# Running crontab -l as root gives:
+# */5 * * * * /root/cron/email-mydata-debug-logs.sh
+
+find /opt/mydata_debug_logs/ -type f -mmin -5 | while read filename
+do
+    # recipients="store.star.help@monash.edu"
+    recipients="james.wettenhall@monash.edu"
+    sender=$( cat $filename | grep Email: | sed 's/Email://' )
+    export REPLYTO="$sender"
+    export EMAIL="$sender"
+    mutt -s "[MyData Debug Log] $filename" $recipients < $filename
+    echo "$(date): Sent MyData debug log $sender $recipients $filename" >> mydata_email.log
+done
---
+++ logger/mydata_log_drop.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# This file, used by MyData, lives in
+# /var/www/cgi-bin/mydata_log_drop.py
+# on cvl.massive.org.au
+
+import cgi, os
+import cgitb; cgitb.enable()
+import string
+import tempfile
+import subprocess
+
+MAX_LOG_DIRECTORY_SIZE  = 100 # size of log directory in Mb
+LOG_DIRECTORY           = '/opt/mydata_debug_logs/'
+
+MAX_NR_BYTES_LOG        = 50*1048576 # 50Mb
+
+
+def free_root_space():
+    proc = subprocess.Popen('df -B MB /',
+                            stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            shell=True,
+                            universal_newlines=True)
+    stdout, stderr = proc.communicate()
+
+    return int(stdout.split('\n')[1].split('MB')[2]) > \
+        1.5 * MAX_LOG_DIRECTORY_SIZE
+
+def free_space():
+    proc = subprocess.Popen('du -B MB ' + LOG_DIRECTORY,
+                            stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            shell=True,
+                            universal_newlines=True)
+    stdout, stderr = proc.communicate()
+
+    return int(stdout.split('\n')[0].split('MB')[0]) < \
+        MAX_LOG_DIRECTORY_SIZE
+
+form = cgi.FieldStorage()
+out = None
+
+if 'logfile' in form:
+    logfile = form['logfile']
+
+    if logfile.file and free_space() and free_root_space():
+        out = tempfile.NamedTemporaryFile(mode='w', dir=LOG_DIRECTORY,
+                                          delete=False)
+
+        BYTES_PER_READ = 1024
+
+        bytes_read = 0
+
+        while True:
+            blob = logfile.file.read(BYTES_PER_READ)
+            if not blob: break
+            bytes_read += BYTES_PER_READ
+
+            if bytes_read >= MAX_NR_BYTES_LOG:
+                out.write("Exceeded limit of %d bytes in upload, exiting.\n"
+                          % MAX_NR_BYTES_LOG)
+                break
+            else:
+                out.write(blob)
+        out.close()
+
+print """
+Content-Type: text/html
+
+<html><body>
+<p>%s</p>
+</body></html>
+""" % ('boo',)
