229b2b8c88770036e147c38e3aa4eb1c75865e26
James Wettenhall <James.Wettenhall@monash.edu>
2015-05-07 15:32:36 +1000

    Removed unused functionality for dragging and dropping dataset folders into MyData.

--- mydata/DragAndDrop.py
+++ mydata/DragAndDrop.py
@@ -1,16 +1,6 @@
 import wx
 
 
-class MyFolderDropTarget(wx.FileDropTarget):
-
-    def __init__(self, parent):
-        wx.FileDropTarget.__init__(self)
-        self.parent = parent
-
-    def OnDropFiles(self, x, y, filenames):
-        self.parent.OnDropFiles(filenames)
-
-
 class MyDataSettingsDropTarget(wx.FileDropTarget):
 
     def __init__(self, parent):
