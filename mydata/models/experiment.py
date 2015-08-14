fdadfca9b0179f7cfb1ddb2e034f6cdd03764072
James Wettenhall <James.Wettenhall@monash.edu>
2015-08-15 03:23:15 +1000

    Experiments are now made accessible to facility manager groups, so facility managers can see their users' experiments in their "Shared" list while logged in as themself, instead of having to log in using a shared facility role account.

--- mydata/models/experiment.py
+++ mydata/models/experiment.py
@@ -265,6 +265,9 @@ class ExperimentModel():
                 message += " and group folder \"%s\"" % groupFolderName
             logger.debug(message)
 
+            facilityManagers = settingsModel.GetFacility().GetManagerGroup()
+            ObjectAclModel.ShareExperimentWithGroup(createdExperiment,
+                                                    facilityManagers)
             # Avoid creating a duplicate ObjectACL if the user folder's
             # username matches the facility manager's username.
             # Don't attempt to create an ObjectACL record for an
@@ -273,7 +276,9 @@ class ExperimentModel():
                     ownerUserId is not None:
                 ObjectAclModel.ShareExperimentWithUser(createdExperiment,
                                                        owner)
-            if folderModel.GetGroup() is not None:
+            if folderModel.GetGroup() is not None and \
+                    folderModel.GetGroup().GetId() != \
+                    facilityManagers.GetGroup().GetId():
                 ObjectAclModel.ShareExperimentWithGroup(createdExperiment,
                                                         folderModel.GetGroup())
         else:
