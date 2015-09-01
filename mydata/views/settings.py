dfb32aa13d705f3af082b3fa20cc7d4668e2b221
James Wettenhall <james.wettenhall@monash.edu>
2015-09-01 22:38:03 +1000

    Allow for both AM/PM and 24-hour time format.  The actual format returned by wx.DataTime.Format() tends to be 24-hour time on Windows and AM/PM on Mac OS X.

--- mydata/views/settings.py
+++ mydata/views/settings.py
@@ -707,7 +707,10 @@ class SettingsDialog(wx.Dialog):
     def GetScheduledTime(self):
         wxDateTime = self.timeCtrl.GetValue(as_wxDateTime=True)
         timeString = wxDateTime.FormatTime()
-        return datetime.time(datetime.strptime(timeString, "%H:%M:%S %p"))
+	try:
+            return datetime.time(datetime.strptime(timeString, "%H:%M:%S %p"))
+        except ValueError:
+            return datetime.time(datetime.strptime(timeString, "%H:%M:%S"))
 
     def SetScheduledTime(self, time):
         timeString = "%d:%d:%d" % (time.hour, time.minute, time.second)
@@ -725,7 +728,10 @@ class SettingsDialog(wx.Dialog):
     def GetTimerFromTime(self):
         wxDateTime = self.fromTimeCtrl.GetValue(as_wxDateTime=True)
         timeString = wxDateTime.FormatTime()
-        return datetime.time(datetime.strptime(timeString, "%H:%M:%S %p"))
+	try:
+            return datetime.time(datetime.strptime(timeString, "%H:%M:%S %p"))
+        except:
+	    return datetime.time(datetime.strptime(timeString, "%H:%M:%S"))
 
     def SetTimerFromTime(self, time):
         timeString = "%d:%d:%d" % (time.hour, time.minute, time.second)
@@ -736,7 +742,10 @@ class SettingsDialog(wx.Dialog):
     def GetTimerToTime(self):
         wxDateTime = self.toTimeCtrl.GetValue(as_wxDateTime=True)
         timeString = wxDateTime.FormatTime()
-        return datetime.time(datetime.strptime(timeString, "%H:%M:%S %p"))
+	try:
+            return datetime.time(datetime.strptime(timeString, "%H:%M:%S %p"))
+        except:
+            return datetime.time(datetime.strptime(timeString, "%H:%M:%S"))
 
     def SetTimerToTime(self, time):
         timeString = "%d:%d:%d" % (time.hour, time.minute, time.second)
