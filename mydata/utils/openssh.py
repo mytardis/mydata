mytardis/mydata

commit e95c644c244d3fe1320ed6593d8e84efb05ba593

Making chunk files hidden (on POSIX systems).

mydata/utils/openssh.py
@@ -388,7 +388,7 @@ def NewKeyPair(keyName=None, keyPath=None, keyComment=None):
         raise SshException(stdout)
 
 
-#pylint: disable=too-many-locals
+# pylint: disable=too-many-locals
 def GetBytesUploadedToStaging(remoteFilePath, username, privateKeyFilePath,
                               hostname):
     if sys.platform.startswith("win"):
@@ -600,7 +600,7 @@ def UploadFileFromPosixSystem(filePath, fileSize, username, privateKeyFilePath,
     and then append the chunk onto the remote (partial) datafile.
     """
 
-    remoteChunkPath = remoteFilePath + ".chunk"
+    remoteChunkPath = "." + remoteFilePath + ".chunk"
 
     # logger.warning("Assuming that the remote shell is Bash.")
 
@@ -956,7 +956,7 @@ def UploadLargeFileFromWindows(filePath, fileSize, username,
     if mkdirProcess.returncode != 0:
         raise SshException(stdout, mkdirProcess.returncode)
 
-    remoteChunkPath = remoteFilePath + ".chunk"
+    remoteChunkPath = "." + remoteFilePath + ".chunk"
 
     # logger.warning("Assuming that the remote shell is Bash.")
 

commit 0ac40621decd6bf4f0062721c16afca59ece2459

Fix for bug introduced in e95c644c244d3fe1320ed6593d8e84efb05ba593

mydata/utils/openssh.py
@@ -605,7 +605,8 @@ def UploadFileFromPosixSystem(filePath, fileSize, username, privateKeyFilePath,
     and then append the chunk onto the remote (partial) datafile.
     """
 
-    remoteChunkPath = "." + remoteFilePath + ".chunk"
+    remoteChunkPath = "%s/.%s.chunk" % (os.path.dirname(remoteFilePath),
+                                        os.path.basename(remoteFilePath))
 
     # logger.warning("Assuming that the remote shell is Bash.")
 
@@ -968,7 +969,8 @@ def UploadLargeFileFromWindows(filePath, fileSize, username,
     if mkdirProcess.returncode != 0:
         raise SshException(stdout, mkdirProcess.returncode)
 
-    remoteChunkPath = "." + remoteFilePath + ".chunk"
+    remoteChunkPath = "%s/.%s.chunk" % (os.path.dirname(remoteFilePath),
+                                        os.path.basename(remoteFilePath))
 
     # logger.warning("Assuming that the remote shell is Bash.")
 