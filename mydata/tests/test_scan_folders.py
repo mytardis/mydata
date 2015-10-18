25bfff2b453f08ad951e102a42ae42a122120b4a
James Wettenhall <James.Wettenhall@monash.edu>
2015-10-19 09:42:28 +1100

    Enabled test for doing the full MyData pipeline (scanning data folders and uploading files).

--- mydata/tests/test_scan_folders.py
+++ mydata/tests/test_scan_folders.py
@@ -2,32 +2,220 @@
 Test ability to scan folders.
 """
 import unittest
-# import logging
-# import wx
-from nose.plugins.skip import SkipTest
+import os
+import sys
+import time
+import logging
+import wx
+import subprocess
+import requests
 
-# from mydata.models.settings import SettingsModel
-# from mydata.dataviewmodels.folders import FoldersModel
+from mydata.models.settings import SettingsModel
+from mydata.dataviewmodels.folders import FoldersModel
+from mydata.dataviewmodels.users import UsersModel
+from mydata.dataviewmodels.groups import GroupsModel
+from mydata.dataviewmodels.uploads import UploadsModel
+from mydata.dataviewmodels.verifications import VerificationsModel
+from mydata.views.folders import FoldersView
+from mydata.controllers.folders import FoldersController
+import mydata.utils.openssh as OpenSSH
+from mydata.models.upload import UploadStatus
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class ScanFoldersTester(unittest.TestCase):
     """
     Test ability to scan folders.
     """
-    def test_folders_view(self):
+    def __init__(self, *args, **kwargs):
+        super(ScanFoldersTester, self).__init__(*args, **kwargs)
+        self.fakeMyTardisServerProcess = None
+        self.fakeSshServerProcess = None
+
+    def setUp(self):
+        self.app = wx.PySimpleApp()
+        self.frame = wx.Frame(parent=None, id=wx.ID_ANY,
+                              title='ScanFoldersTester')
+        self.StartFakeMyTardisServer()
+        self.StartFakeSshServer()
+
+    def tearDown(self):
+        self.frame.Destroy()
+        self.app.Destroy()
+        self.fakeMyTardisServerProcess.terminate()
+        self.fakeSshServerProcess.terminate()
+
+    def test_scan_folders(self):
         """
         Test ability to scan folders.
         """
         # pylint: disable=no-self-use
+        # pylint: disable=too-many-statements
+        # pylint: disable=too-many-locals
+        # pylint: disable=too-many-branches
 
-        raise SkipTest("ScanFoldersTester is not implemented yet.")
+        pathToTestConfig = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "testdata/testdata1.cfg")
+        settingsModel = SettingsModel(pathToTestConfig)
+        settingsModel.SetDataDirectory(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "testdata", "testdata1"))
+        settingsModel.SetUseSshControlMasterIfAvailable(False)
+        print "Waiting for fake MyTardis server to start..."
+        attempts = 0
+        while True:
+            try:
+                attempts += 1
+                requests.get(settingsModel.GetMyTardisUrl() + "/about/",
+                             timeout=1)
+                break
+            except requests.exceptions.ConnectionError, err:
+                print str(err)
+                time.sleep(0.1)
+                if attempts > 10:
+                    raise Exception("Couldn't connect to %s"
+                                    % settingsModel.GetMyTardisUrl())
 
-        # app = wx.App(redirect=False)  # pylint: disable=unused-variable
-        # usersModel = None
-        # groupsModel = None
-        # pathToTestConfig = "???"
-        # settingsModel = SettingsModel(pathToTestConfig)
-        # foldersModel = FoldersModel(usersModel, groupsModel, settingsModel)
-        # foldersModel.ScanFolders()
+        settingsValidation = settingsModel.Validate()
+        assert settingsValidation.IsValid()
+        usersModel = UsersModel(settingsModel)
+        groupsModel = GroupsModel(settingsModel)
+        foldersModel = FoldersModel(usersModel, groupsModel, settingsModel)
+
+        def IncrementProgressDialog():
+            """
+            Callback for ScanFolders.
+            """
+            pass
+
+        def ShouldAbort():
+            """
+            Callback for ScanFolders.
+            """
+            return False
+
+        foldersModel.ScanFolders(IncrementProgressDialog, ShouldAbort)
+        assert sorted(usersModel.GetValuesForColname("Username")) == \
+            ["testuser1", "testuser2"]
+
+        folders = []
+        for row in range(foldersModel.GetRowCount()):
+            folders.append(foldersModel.GetFolderRecord(row).GetFolder())
+        assert sorted(folders) == ["Birds", "Flowers"]
+
+        numFiles = 0
+        for row in range(foldersModel.GetRowCount()):
+            numFiles += foldersModel.GetFolderRecord(row).GetNumFiles()
+        assert numFiles == 5
+
+        uploadsModel = UploadsModel()
+        verificationsModel = VerificationsModel()
+        foldersView = FoldersView(self.frame, foldersModel)
+        foldersController = \
+            FoldersController(self.frame,
+                              foldersModel,
+                              foldersView,
+                              usersModel,
+                              verificationsModel,
+                              uploadsModel,
+                              settingsModel)
+
+        print "Waiting for fake SSH server to start up..."
+        username = "mydata"
+        keyPair = OpenSSH.FindKeyPair("MyData")
+        privateKeyFilePath = keyPair.GetPrivateKeyFilePath()
+        host = "localhost"
+        port = 2200
+        while not OpenSSH.SshServerIsReady(username, privateKeyFilePath,
+                                           host, port):
+            time.sleep(0.1)
+
+        foldersController.StartDataUploads()
+
+        while True:
+            numVerificationsCompleted = verificationsModel.GetCompletedCount()
+            uploadsToBePerformed = uploadsModel.GetRowCount()
+            uploadsCompleted = uploadsModel.GetCompletedCount()
+            uploadsFailed = uploadsModel.GetFailedCount()
+            uploadsProcessed = uploadsCompleted + uploadsFailed
+
+            if numVerificationsCompleted == numFiles and \
+                    foldersController.finishedCountingVerifications.isSet() \
+                    and uploadsProcessed == uploadsToBePerformed:
+                break
+            time.sleep(0.1)
+        assert numVerificationsCompleted == numFiles
+
+        # Verifications won't be able to trigger uploads via PostEvent,
+        # because there is no running event loop, so we'll manually call
+        # UploadDatafile for each datafile.
+
+        uploadsToBePerformed = numVerificationsCompleted
+        for i in range(numVerificationsCompleted):
+            verificationModel = verificationsModel.verificationsData[i]
+            folderModel = verificationModel.GetFolderModel()
+            dataFileIndex = verificationModel.GetDataFileIndex()
+            event = foldersController.didntFindDatafileOnServerEvent(
+                foldersController=foldersController,
+                folderModel=folderModel,
+                dataFileIndex=dataFileIndex,
+                verificationModel=verificationModel)
+            foldersController.UploadDatafile(event)
+
+        print "Waiting for uploads to complete..."
+        logger.debug("Waiting for uploads to complete...")
+        while True:
+            uploadsCompleted = uploadsModel.GetCompletedCount()
+            uploadsFailed = uploadsModel.GetFailedCount()
+            uploadsProcessed = uploadsCompleted + uploadsFailed
+
+            if uploadsProcessed == uploadsToBePerformed:
+                break
+            time.sleep(0.1)
+        foldersController.ShutDownUploadThreads()
+
+        print ""
+        print "%d/%d uploads completed." % (uploadsCompleted, uploadsProcessed)
+        if uploadsFailed > 0:
+            print "%d/%d uploads failed." % (uploadsFailed, uploadsProcessed)
+
+        for i in range(uploadsProcessed):
+            uploadModel = uploadsModel.uploadsData[i]
+            if uploadModel.GetStatus() == UploadStatus.FAILED:
+                print "Upload failed for %s: %s" % \
+                    (uploadModel.GetFilename(),
+                     uploadModel.GetMessage().strip())
+                if uploadModel.GetTraceback():
+                    print uploadModel.GetTraceback()
+        print ""
+
+        assert uploadsCompleted == numFiles
+
+    def StartFakeMyTardisServer(self):
+        """
+        Start fake MyTardis server.
+        """
+        os.environ['PYTHONPATH'] = os.path.realpath(".")
+        self.fakeMyTardisServerProcess = \
+            subprocess.Popen([sys.executable,
+                              "mydata/tests/fake_mytardis_server.py"],
+                             env=os.environ)
+
+    def StartFakeSshServer(self):
+        """
+        Start fake SSH/SCP server.
+        """
+        os.environ['PYTHONPATH'] = os.path.realpath(".")
+        self.fakeSshServerProcess = \
+            subprocess.Popen([sys.executable,
+                              "mydata/tests/fake_ssh_server.py"],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             env=os.environ)
+
+
+if __name__ == '__main__':
+    unittest.main()
