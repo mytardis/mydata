6998776c8c53399836f17b70a4362be2c4cc055b
James Wettenhall <james.wettenhall@monash.edu>
2016-06-08 14:26:32 +1000

    Hyphens in version numbers are not valid for RPMs.

--- linux/package_centos_version.sh
+++ linux/package_centos_version.sh
@@ -3,6 +3,7 @@
 SRC=`pwd`
 
 VERSION=`grep '^__version__' ../mydata/__init__.py | cut -f 2 -d '"'`
+RPM_VERSION=`echo ${VERSION} | tr -d '-'`
 ARCHITECTURE=`uname -m | sed s/x86_64/amd64/g | sed s/i686/i386/g`
 
 ./package_linux_version.sh
@@ -16,24 +17,24 @@ rm -f ~/.rpmmacros
 echo "%_topdir  "`pwd`     >> ~/.rpmmacros
 echo "%_tmppath "`pwd`/tmp >> ~/.rpmmacros
 
-sed s/VERSION/${VERSION}/g ../mydata.spec.template > SPECS/mydata.spec
+sed s/VERSION/${RPM_VERSION}/g ../mydata.spec.template > SPECS/mydata.spec
 
 if [ "$ARCHITECTURE" == "amd64" ]
 then
     sed -i s/libc.so.6\(GLIBC_PRIVATE\)/libc.so.6\(GLIBC_PRIVATE\)\(64bit\)/g SPECS/mydata.spec
 fi
 
-rm -fr mydata-${VERSION}
+rm -fr mydata-${RPM_VERSION}
 
-mkdir -p mydata-${VERSION}/opt/mydata
-mkdir -p mydata-${VERSION}/usr/share/applications
-rm -f mydata-${VERSION}.tar.gz SOURCES/mydata-${VERSION}.tar.gz 
+mkdir -p mydata-${RPM_VERSION}/opt/mydata
+mkdir -p mydata-${RPM_VERSION}/usr/share/applications
+rm -f mydata-${RPM_VERSION}.tar.gz SOURCES/mydata-${RPM_VERSION}.tar.gz 
 
-cp ../MyData.desktop mydata-${VERSION}/usr/share/applications/
-cp -r ../dist/MyData-${VERSION}_${ARCHITECTURE}/* mydata-${VERSION}/opt/mydata
+cp ../MyData.desktop mydata-${RPM_VERSION}/usr/share/applications/
+cp -r ../dist/MyData-${VERSION}_${ARCHITECTURE}/* mydata-${RPM_VERSION}/opt/mydata
 
-tar zcf mydata-${VERSION}.tar.gz mydata-${VERSION}
-cp mydata-${VERSION}.tar.gz SOURCES/
+tar zcf mydata-${RPM_VERSION}.tar.gz mydata-${RPM_VERSION}
+cp mydata-${RPM_VERSION}.tar.gz SOURCES/
 
 rpmbuild -ba SPECS/mydata.spec
 cd ..
