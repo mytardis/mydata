da151de43d73c59bd6113fcd3e18a8fb7d409546
James Wettenhall <james.wettenhall@monash.edu>
2016-06-08 12:59:58 +1000

    Fixes for Linux build scripts.

--- linux/MyData.sh
+++ linux/MyData.sh
@@ -2,13 +2,6 @@
 
 MYDATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -f MyData ]; then
-  LD_LIBRARY_PATH="${MYDATA_DIR}":$LD_LIBRARY_PATH
-  "${MYDATA_DIR}"/MyData
-elif [ -f bin/MyData ]; then
-  LD_LIBRARY_PATH="${MYDATA_DIR}/bin":$LD_LIBRARY_PATH
-  "${MYDATA_DIR}"/bin/MyData
-else
-  echo "ERROR: Cannot find MyData."
-fi
+LD_LIBRARY_PATH="${MYDATA_DIR}/bin":$LD_LIBRARY_PATH
+"${MYDATA_DIR}"/bin/MyData
 
--- linux/package_centos_version.sh
+++ linux/package_centos_version.sh
@@ -7,37 +7,35 @@ ARCHITECTURE=`uname -m | sed s/x86_64/amd64/g | sed s/i686/i386/g`
 
 ./package_linux_version.sh
 
+rm -fr rpmbuild
 mkdir -p rpmbuild
 cd rpmbuild
-
-rm -fr BUILD BUILDROOT RPMS SOURCES SRPMS tmp
-mkdir  BUILD BUILDROOT RPMS SOURCES SRPMS tmp
+mkdir  BUILD BUILDROOT RPMS SOURCES SPECS SRPMS tmp
 
 rm -f ~/.rpmmacros
 echo "%_topdir  "`pwd`     >> ~/.rpmmacros
 echo "%_tmppath "`pwd`/tmp >> ~/.rpmmacros
 
-
-sed s/VERSION/${VERSION}/g SPECS/MyData.spec.template > SPECS/MyData.spec
+sed s/VERSION/${VERSION}/g ../mydata.spec.template > SPECS/mydata.spec
 
 if [ "$ARCHITECTURE" == "amd64" ]
 then
-    sed -i s/libc.so.6\(GLIBC_PRIVATE\)/libc.so.6\(GLIBC_PRIVATE\)\(64bit\)/g SPECS/MyData.spec
+    sed -i s/libc.so.6\(GLIBC_PRIVATE\)/libc.so.6\(GLIBC_PRIVATE\)\(64bit\)/g SPECS/mydata.spec
 fi
 
 rm -fr mydata-${VERSION}
 
-mkdir -p mydata-${VERSION}/opt/MyData
+mkdir -p mydata-${VERSION}/opt/mydata
 mkdir -p mydata-${VERSION}/usr/share/applications
 rm -f mydata-${VERSION}.tar.gz SOURCES/mydata-${VERSION}.tar.gz 
 
 cp ../MyData.desktop mydata-${VERSION}/usr/share/applications/
-cp -r ../dist/MyData-${VERSION}_${ARCHITECTURE}/* mydata-${VERSION}/opt/MyData
+cp -r ../dist/MyData-${VERSION}_${ARCHITECTURE}/* mydata-${VERSION}/opt/mydata
 
 tar zcf mydata-${VERSION}.tar.gz mydata-${VERSION}
 cp mydata-${VERSION}.tar.gz SOURCES/
 
-rpmbuild -ba SPECS/MyData.spec
+rpmbuild -ba SPECS/mydata.spec
 cd ..
 
 find rpmbuild/ -iname '*rpm' -exec ls -lh {} \;
