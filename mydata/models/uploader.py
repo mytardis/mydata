48e7bf5ca531abeca5c6bcd5f82d90a122967cad
James Wettenhall <James.Wettenhall@monash.edu>
2016-07-20 11:50:02 +1000

    Fix for ipconfig parsing bug on Windows.  Sometimes "(Tentative)" appears after an IP address in ipconfig output and MyData was trying to INSERT the IP address plus the "(Tentative)" string into a database field only big enough for an IP address.
    When there are multiple network interfaces, e.g. one for Internet
    and one for a data-collection instrument, MyData tries to guess
    which is the interface used to connect to MyTardis, so that it
    can detect connectivity problems early rather than throwing an
    ugly exception.  Some instrument PCs are set up with interfaces
    named "Internet" and "Instrument", instead of using the more
    common "Ethernet", "Local Area Connection" or
    "Local Area Connection2" interface names.  In this case, MyData
    was incorrectly reporting an Internet connectivity problem
    when the instrument connection was inactive.

--- mydata/models/uploader.py
+++ mydata/models/uploader.py
@@ -171,6 +171,8 @@ class UploaderModel(object):
             activeInterfaces = ["Local Area Connection 2"]
         elif "Ethernet" in activeInterfaces:
             activeInterfaces = ["Ethernet"]
+        elif "Internet" in activeInterfaces:
+            activeInterfaces = ["Internet"]
         elif "Wi-Fi" in activeInterfaces:
             activeInterfaces = ["Wi-Fi"]
 
@@ -210,9 +212,15 @@ class UploaderModel(object):
                         if "IPv4 Address" in key.strip(' .'):
                             ipv4Address[interface] = \
                                 value.strip().replace("(Preferred)", "")
+                            ipv4Address[interface] = \
+                                ipv4Address[interface] \
+                                    .replace("(Tentative)", "")
                         if "IPv6 Address" in key.strip(' .'):
                             ipv6Address[interface] = \
                                 value.strip().replace("(Preferred)", "")
+                            ipv6Address[interface] = \
+                                ipv6Address[interface] \
+                                    .replace("(Tentative)", "")
                         if "Subnet Mask" in key.strip(' .'):
                             subnetMask[interface] = value.strip()
         else:
