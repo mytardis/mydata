5c03a8c252048215c175ad4f08248ecf80e55912
James Wettenhall <James.Wettenhall@monash.edu>
2017-01-21 23:50:46 +1100

    New test for testing handling HTTP errors.

---
+++ mydata/tests/test_handle_http_error.py
@@ -0,0 +1,61 @@
+"""
+Test method for handling HTTP error.
+"""
+import unittest
+import wx
+
+from mydata.models import HandleHttpError
+from mydata.utils.exceptions import Unauthorized
+from mydata.utils.exceptions import DoesNotExist
+from mydata.utils.exceptions import InternalServerError
+from mydata.utils.exceptions import BadGateway
+from mydata.utils.exceptions import ServiceUnavailable
+from mydata.utils.exceptions import GatewayTimeout
+from mydata.utils.exceptions import HttpException
+
+
+class MockResponse(object):
+    """
+    Trivial class to mock the status_code attribute of the
+    responses returned by the requests library.
+    """
+    # pylint: disable=too-few-public-methods
+    def __init__(self, statusCode, text=""):
+        """
+        Initialize a mock response instance with a status code.
+        """
+        self.status_code = statusCode  # pylint: disable=invalid-name
+        self.text = text
+
+
+class HttpErrorHandlerTester(unittest.TestCase):
+    """
+    Test method for handling HTTP error.
+    """
+    def setUp(self):
+        self.app = wx.App(redirect=False)  # pylint: disable=unused-variable
+        self.frame = wx.Frame(None, title='HttpErrorHandlerTester')
+        self.frame.Show()
+
+    def test_handle_http_error(self):
+        """
+        Test method for handling HTTP error.
+        """
+        with self.assertRaises(Unauthorized):
+            HandleHttpError(MockResponse(403))
+        with self.assertRaises(DoesNotExist):
+            HandleHttpError(MockResponse(404))
+        with self.assertRaises(InternalServerError):
+            HandleHttpError(MockResponse(500))
+        with self.assertRaises(BadGateway):
+            HandleHttpError(MockResponse(502))
+        with self.assertRaises(ServiceUnavailable):
+            HandleHttpError(MockResponse(503))
+        with self.assertRaises(GatewayTimeout):
+            HandleHttpError(MockResponse(504))
+        with self.assertRaises(HttpException):
+            HandleHttpError(MockResponse(505))
+
+    def tearDown(self):
+        self.frame.Hide()
+        self.frame.Destroy()
