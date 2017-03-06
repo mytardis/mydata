df8446fa7d779da4bd82d03d3b15026d9568ff8d
James Wettenhall <James.Wettenhall@monash.edu>
2017-03-06 16:35:02 +1100

    Added docs for LastSettingsUpdateTrigger

--- mydata/models/settings/miscellaneous.py
+++ mydata/models/settings/miscellaneous.py
@@ -11,12 +11,23 @@ import sys
 
 class LastSettingsUpdateTrigger(object):
     """
-    Enumerated data type encapsulating the trigger
-    for the settings update, either:
-        LastSettingsUpdateTrigger.READ_FROM_DISK or
-        LastSettingsUpdateTrigger.UI_RESPONSE
+    Enumerated data type encapsulating the trigger for the last change to
+    the settings.
+
+    This is used to determine whether settings validation is required.
+
+    If the user opens the settings dialog, and clicks OK, validation is
+    performed automatically, so there is no need to validate settings
+    again at the beginning of a scan-folders-and-upload task commenced
+    shortly after closing the settings dialog.
+
+    However, if MyData finds a MyData.cfg on disk when it launches, and
+    then the user clicks the "Upload" button without opening the settings
+    dialog first, then we do need to validate settings.
     """
+    # The last update to SETTINGS came from reading MyData.cfg from disk:
     READ_FROM_DISK = 0
+    # The last update to SETTINGS came from the settings dialog:
     UI_RESPONSE = 1
 
 
