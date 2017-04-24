4c8de8841195e971ad545608d7073d3c7db64d13
James Wettenhall <James.Wettenhall@monash.edu>
2017-04-24 17:48:54 +1000

    Fixing online docs test.

--- mydata/tests/miscellaneous/test_mydata_online_docs.py
+++ mydata/tests/miscellaneous/test_mydata_online_docs.py
@@ -1,11 +1,10 @@
 """
 Test ability to access MyData's online documentation.
 """
-import os
-
 import wx
 
-from ...MyData import MyData
+from ...events.docs import OnHelp
+from ...events.docs import OnWalkthrough
 from .. import MyDataMinimalTester
 
 
@@ -13,23 +12,10 @@ class MyDataOnlineDocsTester(MyDataMinimalTester):
     """
     Test ability to access MyData's online documentation.
     """
-    def __init__(self, *args, **kwargs):
-        super(MyDataOnlineDocsTester, self).__init__(*args, **kwargs)
-        self.mydataApp = None
-
-    def setUp(self):
-        super(MyDataOnlineDocsTester, self).setUp()
-        os.environ['MYDATA_DONT_RUN_SCHEDULE'] = 'True'
-
-    def tearDown(self):
-        super(MyDataOnlineDocsTester, self).tearDown()
-        del os.environ['MYDATA_DONT_RUN_SCHEDULE']
-
     def test_mydata_online_docs(self):
         """
         Test ability to access MyData's online documentation.
         """
-        self.mydataApp = MyData(argv=['MyData', '--loglevel', 'DEBUG'])
         pyEvent = wx.PyEvent()
-        self.mydataApp.frame.OnHelp(pyEvent)
-        self.mydataApp.frame.OnWalkthrough(pyEvent)
+        OnHelp(pyEvent)
+        OnWalkthrough(pyEvent)
