5c568f341e1b73e556f20f588c05c7b7df6fc380
James Wettenhall <James.Wettenhall@monash.edu>
2018-05-25 10:33:57 +1000

    test_once_schedule_invalid is now a method in the OnceScheduleTester class, instead of having its own class, thus reducing code duplication.

--- mydata/tests/scheduling/test_once_schedule.py
+++ mydata/tests/scheduling/test_once_schedule.py
@@ -8,12 +8,13 @@ from ...settings import SETTINGS
 from ...MyData import MyData
 from ...dataviewmodels.dataview import DATAVIEW_MODELS
 from ...models.settings.validation import ValidateSettings
+from ...utils.exceptions import InvalidSettings
 from .. import MyDataSettingsTester
 
 
 class OnceScheduleTester(MyDataSettingsTester):
     """
-    Test Once schedule type.
+    Test Once schedule type
     """
     def __init__(self, *args, **kwargs):
         super(OnceScheduleTester, self).__init__(*args, **kwargs)
@@ -32,14 +33,22 @@ class OnceScheduleTester(MyDataSettingsTester):
 
     def test_once_schedule(self):
         """
-        Test Once schedule type.
+        Test Once schedule type
         """
         ValidateSettings()
         self.mydataApp = MyData(argv=['MyData', '--loglevel', 'DEBUG'])
-        # testdataUsernameDataset_POST.cfg has upload_invalid_user_folders = True,
-        # so INVALID_USER/InvalidUserDataset1/InvalidUserFile1.txt is included
-        # in the uploads completed count:
-        uploadsModel = DATAVIEW_MODELS['uploads']
-        self.assertEqual(uploadsModel.GetCompletedCount(), 8)
-        # TO DO: A way of testing that additional tasks are scheduled,
-        # according to the timer interval.
+        self.assertEqual(DATAVIEW_MODELS['uploads'].GetCompletedCount(), 8)
+
+    def test_once_schedule_invalid(self):
+        """
+        Test Once schedule type with invalid date/time
+        """
+        SETTINGS.schedule.scheduledTime = \
+            datetime.time(datetime.now().replace(microsecond=0) -
+                          timedelta(minutes=1))
+        with self.assertRaises(InvalidSettings) as contextManager:
+            ValidateSettings()
+        invalidSettings = contextManager.exception
+        self.assertEqual(invalidSettings.field, "scheduled_time")
+        self.mydataApp = MyData(argv=['MyData', '--loglevel', 'DEBUG'])
+        self.assertEqual(DATAVIEW_MODELS['uploads'].GetCompletedCount(), 0)
