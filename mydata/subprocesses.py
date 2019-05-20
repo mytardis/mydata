2268c6355ad7715fb4b9d7c9aad53e18e4c624bd
James Wettenhall <james.wettenhall@gmail.com>
2019-05-20 13:17:54 +1000

    Subprocesses are no longer used on Windows

--- mydata/subprocesses.py
+++ mydata/subprocesses.py
@@ -1,15 +1,5 @@
 """
 Defines common settings for subprocesses.
 """
-import sys
-import subprocess
-
 DEFAULT_STARTUP_INFO = None
 DEFAULT_CREATION_FLAGS = 0
-if sys.platform.startswith("win"):
-    import win32process
-    DEFAULT_STARTUP_INFO = subprocess.STARTUPINFO()
-    # pylint: disable=protected-access
-    DEFAULT_STARTUP_INFO.dwFlags |= subprocess._subprocess.STARTF_USESHOWWINDOW
-    DEFAULT_STARTUP_INFO.wShowWindow = subprocess.SW_HIDE
-    DEFAULT_CREATION_FLAGS = win32process.CREATE_NO_WINDOW
