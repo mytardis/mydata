7e90563a3a370039ba26924b582fe68d63e771c6
James Wettenhall <james.wettenhall@gmail.com>
2019-05-20 23:14:14 +1000

    Make ConnectivityViewTester inherit from MyDataTester and use environment variable to determine whether running in unit tests

--- mydata/tests/views/test_connectivity_view.py
+++ mydata/tests/views/test_connectivity_view.py
@@ -1,28 +1,17 @@
 """
 Test ability to open connectivity view.
 """
-import unittest
-import wx
-
+from .. import MyDataTester
 from ...utils.exceptions import NoActiveNetworkInterface
 from ...views.connectivity import ReportNoActiveInterfaces
 
 
-class ConnectivityViewTester(unittest.TestCase):
+class ConnectivityViewTester(MyDataTester):
     """
     Test ability to open connectivity view.
     """
-    def setUp(self):
-        self.app = wx.App(redirect=False)
-        self.app.SetAppName('ConnectivityViewTester')
-        self.frame = wx.Frame(None, title='ConnectivityViewTester')
-        self.frame.Show()
-
     def test_connectivity_view(self):
         """Test ability to open connectivity view.
         """
         with self.assertRaises(NoActiveNetworkInterface):
             ReportNoActiveInterfaces()
-
-    def tearDown(self):
-        self.frame.Hide()
