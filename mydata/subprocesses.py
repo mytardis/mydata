489c1df34afbe7aa5c30d2d33a4afcb81cf344c0
James Wettenhall <James.Wettenhall@gmail.com>
2020-02-23 16:16:26 -0800

    Avoid popping up Command Prompt windows when running subprocesses

--- mydata/subprocesses.py
+++ mydata/subprocesses.py
@@ -1,5 +1,15 @@
 """
 Defines common settings for subprocesses.
 """
+import sys
+import subprocess
+
 DEFAULT_STARTUP_INFO = None
 DEFAULT_CREATION_FLAGS = 0
+
+if sys.platform.startswith("win"):
+    import win32process
+    DEFAULT_STARTUP_INFO = subprocess.STARTUPINFO()
+    DEFAULT_STARTUP_INFO.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    DEFAULT_STARTUP_INFO.wShowWindow = subprocess.SW_HIDE
+    DEFAULT_CREATION_FLAGS = win32process.CREATE_NO_WINDOW
